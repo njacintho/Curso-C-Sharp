6a3b19b6815059969ca1dd3afc3d21917d8bcf33
Noemi <noemi.jacin@gmail.com>
2021-12-09 19:20:34 -0300

    Aula de Switch

---
+++ AulaSwitch/AulaSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora = DateTime.Now.Hour;
+            string mensagem = string.Empty;
+            switch (hora)
+            {
+                case 10:
+                case 15:
+                    mensagem = "Hora do intervalo";
+                    break;
+                case 12:
+                    mensagem = "Alomoço";
+                    break;
+                case 19:
+                    mensagem = "Estudos";
+                    break ;
+                default:
+                    mensagem = string.Empty;
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
