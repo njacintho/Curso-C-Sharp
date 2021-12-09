3c77c37130567e4f05aca6fd4d4de03bc76f9133
Noemi <noemi.jacin@gmail.com>
2021-12-09 19:50:54 -0300

    Exercio IF minimo para parcelamento de compras

---
+++ Aula06Ativ01/Aula06Ativ01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06Ativ01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal valorCompra;
+            int numeroParcelas = 3;
+            decimal valorParcela;
+            decimal valorMinimoParcelar = 1000.0m;
+
+            Console.Write("Digite o valor da compra:");
+            valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Valor a vista: {0:c}",valorCompra);
+
+            if(valorCompra > valorMinimoParcelar)
+            {
+                valorParcela = valorCompra / numeroParcelas;
+
+                Console.WriteLine("Pode ser parcelado em {0} vezes. Valor da Parcela:{1:c}",
+                    numeroParcelas,valorParcela);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
