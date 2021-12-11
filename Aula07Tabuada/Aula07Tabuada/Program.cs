fe432b7ee86953ab533140347878b694ad1bb53e
Noemi <noemi.jacin@gmail.com>
2021-12-11 12:11:07 -0300

    Tabuada usando For

---
+++ Aula07Tabuada/Aula07Tabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Digite a tabuada que deseja: ");
+            int tabuada = Convert.ToInt32(Console.ReadLine());
+            int multiplica;
+
+            for(int i = 0; i <= 10; i++)
+            {
+                multiplica = tabuada * i;
+                Console.WriteLine("{0} x {1} = {2}", tabuada,i,multiplica);
+            }
+            Console.ReadLine();
+        }
+    }
+}
