ce8fd205c62d6ded45137e4efaf108cb7508c2c9
Noemi <noemi.jacin@gmail.com>
2021-12-11 12:16:29 -0300

    Tabuada com do-while para continuar executando.

--- Aula07Tabuada/Aula07Tabuada/Program.cs
+++ Aula07Tabuada/Aula07Tabuada/Program.cs
@@ -14,12 +14,26 @@ namespace Aula07Tabuada
             Console.Write("Digite a tabuada que deseja: ");
             int tabuada = Convert.ToInt32(Console.ReadLine());
             int multiplica;
+            string valor = string.Empty;
 
-            for(int i = 0; i <= 10; i++)
-            {
-                multiplica = tabuada * i;
-                Console.WriteLine("{0} x {1} = {2}", tabuada,i,multiplica);
-            }
+
+            do {
+                if(valor != string.Empty)
+                {
+                    tabuada = Convert.ToInt32(valor);
+                }
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    multiplica = tabuada * i;
+                    Console.WriteLine("{0} x {1} = {2}", tabuada, i, multiplica);
+                }
+
+                Console.WriteLine();
+                Console.Write("Digite a tabuada que deseja ou enter para sair: ");
+                valor = Console.ReadLine();
+            }while(valor != string.Empty);
+
             Console.ReadLine();
         }
     }
